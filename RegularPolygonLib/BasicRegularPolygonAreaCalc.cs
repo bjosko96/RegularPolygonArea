12b1141d210d1b6c3ced343e460b410be4bfae71
pl_barbara <203898@edu.p.lodz.pl>
2018-05-21 10:14:27 +0200

    Basic model and business logic

---
+++ RegularPolygonLib/BasicRegularPolygonAreaCalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularPolygonLib
+{
+    class BasicRegularPolygonAreaCalc : IRegularPolygonAreaCalc
+    { 
+        public double CalculateArea(double n, double sideLength)
+        {
+            double area = (1.0 / 4.0) * n * sideLength * sideLength * (1.0 / Math.Tan(Math.PI/n));
+            return area;
+        }
+    }
+}
---
+++ RegularPolygonLib/RegularPolygonAreaCalcFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularPolygonLib
+{
+    /// <summary>
+    /// factory for IRegularPolygonAreaCalc object
+    /// </summary>
+    class RegularPolygonAreaCalcFactory
+    {
+        /// <summary>
+        /// method returning concrete implementation of IRegularPolygonAreaCalc
+        /// </summary>
+        /// <returns>IRegularPolygonAreaCalc</returns>
+        public IRegularPolygonAreaCalc GetRegularPolygonAreaCalc()
+        {
+            //when other interface implementations added - pass a parameter to decide which method to use
+            return new BasicRegularPolygonAreaCalc();
+        }
+    }
+}
